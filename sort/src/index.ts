64912af435a85f58722fcc9e2786656a2f84be0b
Soufiane CHAOUFI <soufiane@apploi.com>
2021-01-07 13:57:06 +0100

    sort app

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world');
+import { NumberCollection } from './NumberCollection';
+import { CharacterCollection } from './CharacterCollection';
+import { Sorter } from './Sorter';
+import { LinkedList } from './LinkedList';
+
+const numberCollection = new NumberCollection([50, 10, 3, -5, 0]);
+const characterCollection = new CharacterCollection('Xaayb');
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+numberCollection.sort();
+characterCollection.sort();
+linkedList.sort();
+
+console.log(numberCollection.data);
+console.log(characterCollection.data);
+linkedList.print();
