a200ac5196d96a87bfc984210bb76ce1bd2ac73b
Soufiane CHAOUFI <soufiane@apploi.com>
2021-01-10 22:29:28 +0100

    typescript react action type redux

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,37 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-  color?: string;
-}
-interface AppState {
-  count: number;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-const AppFnc = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
-class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { count: 0 };
-  }
-  onIncrement = (): void => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  onDecrement = (): void => {
-    this.setState({ count: this.state.count - 1 });
-  };
+ReactDOM.render(
+  <Provider store={store}>
+    <App color="red" />
+  </Provider>,
+  document.getElementById('root')
+);
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>increment</button>
-        <button onClick={this.onDecrement}>decrement</button>
-        {this.state.count}
-      </div>
-    );
-  }
-}
+// interface AppProps {
+//   color?: string;
+// }
+// interface AppState {
+//   count: number;
+// }
 
-ReactDOM.render(<App color="red" />, document.getElementById('root'));
+// const AppFnc = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
+// class App extends React.Component<AppProps, AppState> {
+//   constructor(props: AppProps) {
+//     super(props);
+//     this.state = { count: 0 };
+//   }
+//   onIncrement = (): void => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+//   onDecrement = (): void => {
+//     this.setState({ count: this.state.count - 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>increment</button>
+//         <button onClick={this.onDecrement}>decrement</button>
+//         {this.state.count}
+//       </div>
+//     );
+//   }
+// }
