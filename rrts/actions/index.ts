e29c3d3ad0bcc08b984d818a9c6001901bed2ec0
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2021-01-23 22:22:59 +0100

    react ts

--- rrts/actions/index.ts
+++ rrts/actions/index.ts
@@ -1,17 +1,22 @@
-import { ActionType } from './types';
-import axios from 'axios';
-import { Dispatch } from 'redux';
-const url = 'https://jsonplaceholder.typicode.com/todos';
+import { ActionType } from "./types";
+import axios from "axios";
+import { Dispatch } from "redux";
+const url = "https://jsonplaceholder.typicode.com/todos";
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   completed: boolean;
 }
+
+export interface fetchTodosAction {
+  type: ActionType.fetchTodos;
+  payload: Todo[];
+}
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
-    dispatch({
+    dispatch<fetchTodosAction>({
       type: ActionType.fetchTodos,
       payload: response.data,
     });
